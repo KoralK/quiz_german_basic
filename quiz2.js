937e3f7e101a09da32bc3ea4556cf8aa8f5894fb
Koral <koral@Korals-MacBook-Air.local>
2023-08-11 17:48:52 +0300

    quiz2.js connected and fixed

--- quiz2.js
+++ quiz2.js
@@ -14,17 +14,38 @@ document.addEventListener('DOMContentLoaded', function() {
             .catch(error => {
                 console.error('Error:', error);
             });
-    }    
+    }
 
     function displayQuizQuestion() {
-        console.log("currentQuestionIndex:", currentQuestionIndex); // Add this line
-    
+        console.log("currentQuestionIndex:", currentQuestionIndex);
+
         if (currentQuestionIndex >= quizData.length) {
             alert('Quiz finished!');
             return;
         }
+
+        let questionData = quizData[currentQuestionIndex];
+        let questionText = document.getElementById('question-text');
+        questionText.textContent = questionData.question;
+
+        let optionsDiv = document.getElementById('options');
+        optionsDiv.innerHTML = '';
+
+        questionData.options.forEach((option, index) => {
+            let checkBox = document.createElement('input');
+            checkBox.type = 'checkbox';
+            checkBox.id = 'option' + index;
+            checkBox.value = index;
+
+            let label = document.createElement('label');
+            label.htmlFor = 'option' + index;
+            label.appendChild(document.createTextNode(option));
+
+            optionsDiv.appendChild(checkBox);
+            optionsDiv.appendChild(label);
+            optionsDiv.appendChild(document.createElement('br'));
+        });
     }
-    
 
     function hasAnswerSelected() {
         let questionData = quizData[currentQuestionIndex];
@@ -41,33 +62,28 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     document.getElementById('check-answer').addEventListener('click', function() {
-        // ... (rest of the code remains unchanged)
-    });
-
-    document.getElementById('check-answer').addEventListener('click', function() {
         let questionData = quizData[currentQuestionIndex];
         let selectedOptions = [];
-    
+
         questionData.options.forEach((option, index) => {
             let checkBox = document.getElementById('option' + index);
             if (checkBox.checked) {
                 selectedOptions.push(index);
             }
         });
-    
+
         if (JSON.stringify(selectedOptions) === JSON.stringify(questionData.answers)) {
             score++;
         } else {
             score = Math.max(score - 1/3, 0);
             alert('Incorrect. The correct answer is: ' + questionData.answers.map(answerIndex => questionData.options[answerIndex]).join(', '));
         }
-    
+
         document.getElementById('score').textContent = 'Score: ' + score;
-    
+
         currentQuestionIndex++;
         displayQuizQuestion();
     });
-    
 
     document.getElementById('next-question').addEventListener('click', function() {
         if (!hasAnswerSelected()) {
